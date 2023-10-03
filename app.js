0c47857700d20b7969292fd05dfa9a9433377d94
tsaiiii08 <lintsaiting0824@gamil.com>
2023-10-03 22:33:18 +0800

    feat: add user model

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const {pages,apis} = require('./routes')
 const exphbs = require('express-handlebars')
 const app = express()
 const port = process.env.PORT || 3000
+const db = require('./models')
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(pages)
---
+++ migrations/20231003131709-create-user.js
@@ -0,0 +1,56 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      avatar: {
+        type: Sequelize.STRING
+      },
+      introduction: {
+        type: Sequelize.TEXT
+      },
+      nation: {
+        type: Sequelize.STRING
+      },
+      is_teacher: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      },
+      is_admin: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users')
+  }
+}
