6cda3a7843e4fea2d4808ce7d836dcc953620f3a
tsaiiii08 <lintsaiting0824@gamil.com>
2023-10-04 19:28:31 +0800

    feat: add sign up page

--- controllers/pages/user-controller.js
+++ controllers/pages/user-controller.js
@@ -1,7 +1,10 @@
 const userController = {
   signInPage: (req, res) => {
     res.render('signin')
-  }
+  },
+  signUpPage: (req, res) => {
+  res.render('signup')
+},
 }
 
 module.exports = userController
--- routes/pages/index.js
+++ routes/pages/index.js
@@ -5,6 +5,7 @@ const adminController = require('../../controllers/pages/admin-controller')
 const userController = require('../../controllers/pages/user-controller')
 router.get('/admin/users', adminController.getUsers)
 router.get('/users/signIn', userController.signInPage)
+router.get('/users/signUp', userController.signUpPage)
 router.get('/lessons', lessonController.getLessons)
 router.use('/', (req, res) => res.redirect('/lessons'))
 
