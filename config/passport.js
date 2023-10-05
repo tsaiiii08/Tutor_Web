2b7dfb2c1cc27dbb8310bed65caa3b4e1dc472fd
tsaiiii08 <lintsaiting0824@gamil.com>
2023-10-05 13:14:47 +0800

    feat: add google strategy

--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,9 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 const bcrypt = require('bcryptjs')
 const { User } = require('../models')
-// set up Passport strategy
+// set up Passport local strategy
 passport.use(new LocalStrategy(
   // customize user field
   {
@@ -21,6 +22,32 @@ passport.use(new LocalStrategy(
         })
       })
   }
+))
+// set up Passport google strategy
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: process.env.GOOGLE_CALLBACK
+},
+function (accessToken, refreshToken, profile, cb) {
+  const { name, email } = profile._json
+  User.findOne({ where: { email } })
+    .then((user) => {
+      console.log(user)
+      if (user) return cb(null, user)
+      const randomPassword = Math.random().toString(36).slice(-8)
+      bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(randomPassword, salt))
+        .then(hash => User.create({
+          name,
+          email,
+          password: hash
+        }))
+        .then(user => cb(null, user))
+        .catch(err => cb(err, false))
+    })
+}
 ))
 // serialize and deserialize user
 passport.serializeUser((user, cb) => {
--- routes/pages/index.js
+++ routes/pages/index.js
@@ -7,6 +7,11 @@ const userController = require('../../controllers/pages/user-controller')
 const { generalErrorHandler } = require('../../middleware/error-handler')
 const { authenticated, authenticatedAdmin } = require('../../middleware/auth')
 router.get('/admin/users', authenticatedAdmin, adminController.getUsers)
+router.get('/auth/google', passport.authenticate('google', {
+  scope: ['https://www.googleapis.com/auth/userinfo.profile',
+    'https://www.googleapis.com/auth/userinfo.email'] }))
+router.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/users/signIn', failureFlash: true }), userController.signIn)
 router.post('/users/signIn', passport.authenticate('local', { failureRedirect: '/users/signIn', failureFlash: true }), userController.signIn)
 router.get('/users/signIn', userController.signInPage)
 router.get('/users/signUp', userController.signUpPage)
