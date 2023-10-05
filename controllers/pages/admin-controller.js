0d854fa8869e3f8c7282a09fe8e01f654d8e7a12
tsaiiii08 <lintsaiting0824@gamil.com>
2023-10-05 18:27:24 +0800

    feat: admin can view users list

--- controllers/pages/admin-controller.js
+++ controllers/pages/admin-controller.js
@@ -1,6 +1,22 @@
+const { User } = require('../../models')
 const adminController = {
   getUsers: (req, res) => {
-    res.render('admin/users')
+    User.findAll({
+      where: { isAdmin: false },
+      raw: true
+
+    })
+      .then((users) => {
+        users.forEach(user => {
+          if (user.isTeacher === 0) {
+            user.identity = '學生'
+          } else {
+            user.identity = '老師'
+          }
+        })
+        console.log(users)
+        res.render('admin/users', { users })
+      })
   }
 }
 
