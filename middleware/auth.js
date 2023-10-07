82a3dc62231060b9c97ad0c84cc444ea299df256
tsaiiii08 <lintsaiting0824@gamil.com>
2023-10-07 15:29:09 +0800

    feat: add auth for user to becTeacher

--- middleware/auth.js
+++ middleware/auth.js
@@ -13,7 +13,17 @@ const authenticatedAdmin = (req, res, next) => {
     res.redirect('users/signIn')
   }
 }
+const authenticatedStudent = (req, res, next) => {
+  if (ensureAuthenticated(req)) {
+    if (!(getUser(req).isTeacher)) return next()
+    req.flash('error_messages', '你已經是老師身份了!')
+    res.redirect('/lessons')
+  } else {
+    res.redirect('users/signIn')
+  }
+}
 module.exports = {
   authenticated,
-  authenticatedAdmin
+  authenticatedAdmin,
+  authenticatedStudent
 }
