9c34b1db134d0715bcab52a710cbef97ba950e34
tsaiiii08 <lintsaiting0824@gamil.com>
2023-10-11 21:13:42 +0800

    feat: add pagination on lessons page with search keyword

--- controllers/pages/lesson-controller.js
+++ controllers/pages/lesson-controller.js
@@ -1,13 +1,26 @@
 const { Lesson, User } = require('../../models/index')
+const { getOffset, getPagination } = require('../../helpers/pagination-helper')
 const lessonController = {
-  getLessons: (req, res) => {
-    Lesson.findAll({ include: User, raw: true, nest: true })
-      .then((lessons) => {
+  getLessons: (req, res, next) => {
+    const keyword = req.query.keyword || ''
+    const DEFAULT_LIMIT = 6
+    const page = Number(req.query.page) || 1
+    const limit = DEFAULT_LIMIT
+    const offset = getOffset(limit, page)
+    Lesson.findAndCountAll({ include: User, limit, offset, raw: true, nest: true })
+      .then((lessonsAll) => {
+        const lessons = lessonsAll.rows.filter((lesson) =>
+          lesson.name.toLowerCase().includes(keyword.toLowerCase()) || lesson.User.name.toLowerCase().includes(keyword.toLowerCase())
+        )
         lessons.forEach(lesson => {
           lesson.introduction = lesson.introduction?.substring(0, 20)
         })
-        res.render('lessons', { lessons })
+        console.log(getPagination(limit, page, lessons.length))
+        res.render('lessons', {
+          lessons, keyword, pagination: getPagination(limit, page, lessonsAll.count)
       })
+      })
+      .catch(err => next(err))
   }
 }
 
